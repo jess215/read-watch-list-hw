2c8178129487f51c3f1022829daab27de658f59d
Jessica Nguyen <jessica.stevens2115@gmail.com>
2022-02-22 14:30:32 -0700

    added readingList.js

---
+++ readingList.js
@@ -0,0 +1,13 @@
+// readingList = [{id: number, name: string, read: boolean}]
+
+// CRUD
+
+// CREATE: add a book to the reading list
+// Do not mutate readingList
+
+// READ:SKIP (NEED REACT)
+
+// a new updated list when read toggled
+
+// DELETE
+// a new updated list when deleted
